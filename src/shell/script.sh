0a1e07be06e233c0ea3d8e53643d6d0cbbdd954a
Benoit Simard <contact@bsimard.com>
2020-05-13 11:42:27 +0200

    Correct script.sh

--- src/shell/script.sh
+++ src/shell/script.sh
@@ -3,7 +3,7 @@
 ###############################################################################
 # Strict mode
 ###############################################################################
-set -euo pipefail
+set -eo pipefail
 IFS=$'\n\t'
 
 ###############################################################################
@@ -12,15 +12,15 @@ IFS=$'\n\t'
 mkdir -p /var/www/html
 LOG_FILE="/var/www/html/install.log"
 
-echo "Starting script at $(date)" > $LOG_FILE
+echo "Starting script at $(date)" > $LOG_FILE 2>&1
 
 echo
-echo "Installing curl" >> $LOG_FILE
-sudo apt-get update >> $LOG_FILE
+echo "Installing curl" >> $LOG_FILE 2>&1
+sudo apt-get update >> $LOG_FILE 2>&1
 sudo apt-get install -y curl
 
 echo
-echo "Download install script" >> $LOG_FILE
+echo "Download install script" >> $LOG_FILE 2>&1
 curl -s https://raw.githubusercontent.com/medialab/hyphe_openstack_client/master/src/shell/install.sh > install.sh
 chmod +x install.sh
 
@@ -29,20 +29,18 @@ touch /hyphe.env
 # @@_HYPHE_CONFIG_@@
 
 echo
-echo "Loading env"
+echo "Loading env" >> $LOG_FILE 2>&1
 source /hyphe.env
-check $?
 # see https://docs.docker.com/compose/reference/envvars/#compose_file
-sudo echo "export COMPOSE_FILE=$COMPOSE_FILE" > /etc/profile.d/hyphe.sh
-check $?
+sudo echo "#!/bin/sh" > /etc/profile.d/hyphe.sh
+sudo echo "export COMPOSE_FILE=$COMPOSE_FILE" >> /etc/profile.d/hyphe.sh
 chmod +x /etc/profile.d/hyphe.sh
-check $?
 
 echo
-echo "Executing install script" >> $LOG_FILE
+echo "Executing install script" >> $LOG_FILE 2>&1
 ./install.sh >> $LOG_FILE 2>&1
 if [ $? -eq 0 ]; then
-  echo "Installation completed at $(date)" >> $LOG_FILE
+  echo "Installation completed at $(date)" >> $LOG_FILE 2>&1
 else
-  echo "/!\ Installation failed" >> $LOG_FILE
+  echo "/!\ Installation failed" >> $LOG_FILE 2>&1
 fi
